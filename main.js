5d2e82b5a7d2b28254ea0539397cdc27c65abb02
Zaz Brown <zazbrown@zazbrown.com>
2019-04-07 08:09:14 -0400

    + variable sliders

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+// Estimates for stock market statistics:
+const rf = .02 // risk-free rate of return
+const rp = .08 // risk premium
+const vol = .18 // volatility
+
 const lnormc = (x, mu=1, sd=1) =>
 	// CDF(x) of lognormal(mean, standard deviation)
 	1/2 + 1/2*math.erf( (math.log(x)-mu)/2**.5/sd )
 
-const stock = (mu, v) =>
-	// takes the expected return (e.g. 1.1) and volitility of a stock
+const stock = (mu, v, t=1) =>
+	// takes the expected return (e.g. 1.1), volitility of a stock, and duration
 	// returns a cumulative density fn: the lognormal approximation for the stock
-	x => lnormc(x, math.log(mu) - v/2, v**.5)
+	x => lnormc(x, (math.log(mu) - v**2/2)*t, v*t**.5)
+
+const max99 = (mu, v, t=1) => {
+	// find the maximum growth of investment (99.9% chance less than this)
+	sl = stock(mu, v, t)
+	for (let i of Array(9000).keys()) {
+		if (i < 1) { continue }
+		if (sl(i) > .95) {
+			return i
+		}
+	}
+}
 
 $( () => {
 
@@ -15,7 +31,7 @@ let x = [...Array(31)].map((_,i) => i/10)
 //let y = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 1.1]
 //let d = x.map(x => lnormc(x, .1, .18))
 let d = x.map(s)
-let myChart = new Chart(ctx, {
+let chart = new Chart(ctx, {
 	type: 'line',
 	data: {
 		labels: x,
@@ -33,6 +49,28 @@ let myChart = new Chart(ctx, {
 			}]
 		}
 	}
+})
+
+$("input[type='range']").change(function() {
+	el = $(this)
+	el.prev("output").text(el.val())
+
+	let risk = $("#risk").val()
+	let years = $("#years").val()
+
+	let max = max99(1+rf+rp*risk, vol*risk, years)
+	console.log(max)
+	let m = 10
+	if (max > 100) {
+		m = .5
+	} else if (max > 10) {
+		m = 1
+	}
+	let x = [...Array(Math.round(max*m)+1)].map((_,i) => i/m)
+	let s = stock(1.02 + .08*risk, .18*risk, years)
+	chart.data.labels = x
+	chart.data.datasets[0].data = x.map(s)
+	chart.update()
 })
 
 })
